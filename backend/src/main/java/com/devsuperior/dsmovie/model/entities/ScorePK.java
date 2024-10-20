684a69cb1e179427f97bd0fec91ae61a4e32fdd5
Lucas Bernadino <luucasdinoo@gmail.com>
2024-10-20 16:09:34 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/model/entities/ScorePK.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dsmovie.model.entities;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ScorePK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public ScorePK(){}
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScorePK scorePK = (ScorePK) o;
+        return Objects.equals(movie, scorePK.movie) && Objects.equals(user, scorePK.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movie, user);
+    }
+}
